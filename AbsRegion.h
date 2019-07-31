bitblaze-fuzzball/d-s-se-directed-tests

commit 34fcf9a7f64bb68fa1aff0197e9b75cd948352d1

Minor enhancements for easier debugging of static analysis
Put the initial stack pointer value in an easier-to-find place, so
that it can be set to a concrete value for better comparison with
concrete execution.

Improve tracing of loads and stores at debug level 4.

Print constant strided intervals in a more compact format.

AbsRegion.h
@@ -197,7 +197,7 @@ public:
     int getSize() const { return rbt->treeSize(); }
     // Actual range of valid sizes of the memory region
     StridedIntervalPtr getSizeRange() const { return size; }
-    void print(std::ostream&) const;
+    void print(std::ostream&, bool multi_line = false) const;
 
     // Implemented as friends for symmetry
     template <typename T2>
@@ -393,6 +393,28 @@ inline typename Region<T>::VSetPtr Region<T>::read(TPtr p) const {
     
     assert(p->getStride() > 0 && "Addresses can't have zero strides.");
 
+    if (DEBUG_LEVEL >= 4) {
+	if (isRegister()) {
+	    std::stringstream reg_ss;
+            const char* regName = getRegNameAtAddress(p->getLo(),
+                    p->getHi());
+            if (regName) {
+                reg_ss << regName;
+            } else {
+                reg_ss << *p;
+            }
+	    debug4("Interpreting read from register %s\n",
+		   reg_ss.str().c_str());
+	} else {
+	    std::stringstream region_ss;
+	    print(region_ss, true);
+	    std::stringstream pointer_ss;
+	    p->print(pointer_ss);
+	    debug4("Interpreting read from pointer %s in region %s\n",
+		   pointer_ss.str().c_str(), region_ss.str().c_str());
+	}
+    }
+
     if (!size->subsumes(*p)) {
       WARNING("*** Read out of bounds.");
         return ValueSet<T>::getBot();
@@ -543,6 +565,28 @@ inline typename Region<T>::RegionPtr Region<T>::write(TPtr p,VSetPtr s){
     assert(checkWrite(p, s) && "Invalid write.");
 
     assert(p->getStride() > 0 && "Addresses can't have zero strides.");
+
+    if (DEBUG_LEVEL >= 4) {
+	if (isRegister()) {
+	    std::stringstream reg_ss;
+            const char* regName = getRegNameAtAddress(p->getLo(),
+                    p->getHi());
+            if (regName) {
+                reg_ss << regName;
+            } else {
+                reg_ss << *p;
+            }
+	    debug4("Interpreting write to register %s\n",
+		   reg_ss.str().c_str());
+	} else {
+	    std::stringstream region_ss;
+	    print(region_ss, true);
+	    std::stringstream pointer_ss;
+	    p->print(pointer_ss);
+	    debug4("Interpreting write to pointer %s in region %s\n",
+		   pointer_ss.str().c_str(), region_ss.str().c_str());
+	}
+    }
 
     if (!size->subsumes(*p) || p->isTop()) {
         WARNING("*** Write out of bounds");
@@ -1186,7 +1230,7 @@ inline int Region<T>::getHash() const {
 }
 
 template <typename T>
-inline void Region<T>::print(std::ostream& os) const {
+    inline void Region<T>::print(std::ostream& os, bool multi_line) const {
     os << "Region_" << getId() << '_';
     switch (getRegionType()) {
         case rg::WEAK_GLOBAL:
@@ -1237,7 +1281,10 @@ inline void Region<T>::print(std::ostream& os) const {
         const_iterator J = I;
         ++J;
         if (J != E) {
-            os << " ";
+	    if (multi_line)
+		os << std::endl;
+	    else
+		os << " ";
         }
     }
 }

AbsDomStridedInterval.cpp
@@ -1111,28 +1111,37 @@ bool StridedInterval::containsZero() const {
 void StridedInterval::print(std::ostream& os) const {
     const int width = sizeof(int) * 2;
     os << std::dec << std::noshowbase << strd << '[';
-    if (lo == INT_MIN) {
-        os << "-INFTY";
-    } else if (lo == INT_MAX) {
-        os << "+INFTY";
-    } else {
+    if (isConstant()) {
+	// Special case for constants: print only the value
 #ifndef SHOWDECIMAL
         os << std::hex << std::showbase << lo;
 #else
         os << lo;
 #endif
-    }
-    os << ',';
-    if (hi == INT_MIN) {
-        os << "-INFTY";
-    } else if (hi == INT_MAX) {
-        os << "+INFTY";
     } else {
+	if (lo == INT_MIN) {
+	    os << "-INFTY";
+	} else if (lo == INT_MAX) {
+	    os << "+INFTY";
+	} else {
 #ifndef SHOWDECIMAL
-        os << std::hex << std::showbase << hi;
+	    os << std::hex << std::showbase << lo;
 #else
-        os << hi;
+	    os << lo;
 #endif
+	}
+	os << ',';
+	if (hi == INT_MIN) {
+	    os << "-INFTY";
+	} else if (hi == INT_MAX) {
+	    os << "+INFTY";
+	} else {
+#ifndef SHOWDECIMAL
+	    os << std::hex << std::showbase << hi;
+#else
+	    os << hi;
+#endif
+	}
     }
     os << std::dec << std::noshowbase << ']';
 }

AbsState.h
@@ -180,22 +180,29 @@ inline BOOSTPTR AbsState<T>::getInitForMain() {
     RegionPtr r = RegionType::getFresh(rg::STRONG_REGISTER);
     RegionPtr l = RegionType::getFresh(rg::STRONG_STACK);
 
-    // Initialize EBP and ESP to point to zero-offset stack
-    r = r->write(reg::ESP_REG, std::make_pair(l, T::get(0,4,4)));
-    r = r->write(reg::EBP_REG, std::make_pair(l, T::get(0,4,4)));
+    unsigned esp = Prog::getDefaultInitialStackPtr();
+
+    // Initialize ESP to a typical constant
+    assert(esp % 4 == 0 &&
+            "Default stack address not properly aligned.");
+    r = r->write(reg::ESP_REG, std::make_pair(l, T::get(esp, 4)));
+
+    // Initialize EBP to 0, meaning no previous stack frame
+    r = r->write(reg::EBP_REG, std::make_pair(l, T::get(0,4)));
+
     // Initialize registers to zero
-    r = r->write(reg::EAX_REG, std::make_pair(g, T::get(0,4,4)));
-    r = r->write(reg::EBX_REG, std::make_pair(g, T::get(0,4,4)));
-    r = r->write(reg::ECX_REG, std::make_pair(g, T::get(0,4,4)));
-    r = r->write(reg::EDX_REG, std::make_pair(g, T::get(0,4,4)));
-    r = r->write(reg::ESI_REG, std::make_pair(g, T::get(0,4,4)));
-    r = r->write(reg::EDI_REG, std::make_pair(g, T::get(0,4,4)));
+    r = r->write(reg::EAX_REG, std::make_pair(g, T::get(0,4)));
+    r = r->write(reg::EBX_REG, std::make_pair(g, T::get(0,4)));
+    r = r->write(reg::ECX_REG, std::make_pair(g, T::get(0,4)));
+    r = r->write(reg::EDX_REG, std::make_pair(g, T::get(0,4)));
+    r = r->write(reg::ESI_REG, std::make_pair(g, T::get(0,4)));
+    r = r->write(reg::EDI_REG, std::make_pair(g, T::get(0,4)));
     // Initialize return addr, argc, argv to (global,top)
-    l = l->write(T::get(0,4,4), std::make_pair(g, T::getTop()));
-    l = l->write(T::get(4,8,4), std::make_pair(g, T::getTop()));
+    l = l->write(T::get(esp+0,4), std::make_pair(g, T::getTop()));
+    l = l->write(T::get(esp+4,4), std::make_pair(g, T::getTop()));
     assert(Prog::getDefaultArgvAddress() % 4 == 0 &&
             "Argv array address not properly aligned.");
-    l = l->write(T::get(8,12,4), std::make_pair(g,
+    l = l->write(T::get(esp+8,4), std::make_pair(g,
                 T::get(Prog::getDefaultArgvAddress(), 4)));
     RBTreePtr t = RBTree::get(g);
     t = t->insert(r);

prog.h
@@ -176,8 +176,37 @@ public:
 	return &callgraph;
     }
 
+    /* Because the stack has its own memory region, it shouldn't be
+       important for correct static analysis that offsets within the
+       stack region match up with the concrete addresses used for the
+       stack when a program is executing regularly. Therefore the
+       historical behavior is to start the stack offsets from 0; this
+       means that most stack offsets will be small negative numbers.
+       For debugging, it can also be useful to set the initial stack
+       offset to match the initial stack offset that a program has in
+       concrete execution, so that stack operations can easily be
+       compared. */
+    static addr_t getDefaultInitialStackPtr() {
+	// Arbitrary, but typical for 32-bit Linux/x86 with a 32-bit kernel
+        // return 0xbfffc420;
+	// Example of an initial stack address in main() from an
+	// execution under FuzzBALL:
+        // return 0xbfffcf78;
+	// Historical default:
+	return 0;
+    }
+
+    /* In real execution, the argument pointers and the argument
+       strings are stored in higher (older) parts of the stack section
+       that used for stack frames. Currently the static analysis
+       instead creates and argv region as a global region; the
+       important thing there is probably just that its address not
+       overlap with any of the program's static code or data
+       areas.  */
     static addr_t getDefaultArgvAddress() {
-        return 8; // Arbitrary low number
+	// Typically arguments are stored somewhat higher on the stack
+	// than the initial stack pointer
+        return getDefaultInitialStackPtr() + 0x1000;
     }
 
     void createSection(const addr_t a, const char* name, 

commit 8ebe4cff541101c2a391173549ea7819e66d8087

Fix upper-bound calculation in argv-region special case
Without this, the location change in 34fcf9a7 broke the detection of
memory accesses to argv pointers and argv strings; the old code had
worked when the lower bound happened to be almost 0.

AbsRegion.h
@@ -468,9 +468,16 @@ inline typename Region<T>::VSetPtr Region<T>::read(TPtr p) const {
                 // analysis.
                 const size_t lower =
                     mmap->getLowerBound((unsigned)p->getLo());
-                const size_t upper =
+                const size_t upper = lower +
                     NARGV_PTRS_DEFAULT * 4  +
                     NARGV_PTRS_DEFAULT * NARGV_STRLEN_DEFAULT;
+
+		if (DEBUG_LEVEL >= 4) {
+		    debug4("Comparing ptr range [0x%x,0x%x] "
+			   "to argv area [0x%x,0x%x]\n",
+			   (int)p->getLo(), (int)p->getHi(),
+			   (int)lower, (int)upper);
+		}
 
                 if (p->getLo() >= (int)lower && p->getHi() <=
                         (int)upper) {